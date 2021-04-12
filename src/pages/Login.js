8ebf38bcfd966251018f1837f7374b08fc067a97
Blankeos <carloantonioct@gmail.com>
2021-04-12 19:18:03 +0800

    Styled some of these components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,16 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 // Goal is to have input fields for email + password
 // buttons for login, register, login w/ google account
 
 const Login = () => {
-  return <h1 className="text-4xl text-blue-500">Login</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl text-blue-500">Login</h1>;
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
