c0c88073f45acf8c482cdea83e00698c2c50ac7c
Blankeos <carloantonioct@gmail.com>
2021-04-14 22:27:04 +0800

    Finalized color extensions for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: "#e89700", // our primary orange
+        brown: {
+          100: "#c09A6D",
+          200: "#573F27",
+          300: "#3b2d26", // my fave brown
+        },
+        darkGray: "#282f42",
         myOrange: "#ffa500",
       },
     },
