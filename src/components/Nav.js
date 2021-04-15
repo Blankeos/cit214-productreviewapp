ff1554a35bde9e9ef6fdc087e97de9f03b66853c
Blankeos <carloantonioct@gmail.com>
2021-04-15 19:11:21 +0800

    Fixed the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
-
+import Logo from "../assets/imgs/cafely_logo.svg";
 const Nav = () => {
   const { logout, currentUser } = useAuth();
 
@@ -12,35 +12,42 @@ const Nav = () => {
 
   return (
     <div>
-      <nav className="flex">
-        <div className="flex justify-between mx-10">
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/"> Home</Link>
-            </li>
-            <li>
-              <Link to="/review"> Review </Link>
-            </li>
-            <li>
-              <Link to="/products"> Products </Link>
-            </li>
-            <li>
-              <Link to="/about"> About </Link>
-            </li>
-          </ul>
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/login"> Login </Link>
-            </li>
-            <li>
-              <Link to="/register"> Register </Link>
-            </li>
-            {currentUser && (
+      <nav>
+        <div className="flex justify-between items-center mx-10 py-8">
+          <div className="flex space-x-10 items-center">
+            <Link to="/">
+              <img className="w-20" src={Logo} />
+            </Link>
+            <ul className="flex space-x-6">
               <li>
-                <Link onClick={handleLogout}> Logout </Link>
+                <Link to="/"> Home</Link>
               </li>
-            )}
-          </ul>
+              <li>
+                <Link to="/review"> Review </Link>
+              </li>
+              <li>
+                <Link to="/products"> Products </Link>
+              </li>
+              <li>
+                <Link to="/about"> About </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="flex space-x-6">
+              <li>
+                <Link to="/login"> Login </Link>
+              </li>
+              <li>
+                <Link to="/register"> Register </Link>
+              </li>
+              {currentUser && (
+                <li>
+                  <Link onClick={handleLogout}> Logout </Link>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
