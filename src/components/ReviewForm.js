2dba02f0459c819779b1ac144d12d54fe5d1e0a5
Blankeos <carloantonioct@gmail.com>
2021-04-17 21:53:00 +0800

    Polished with working state values for the star rating review form. I have yet to make it integrate seamlessly to one post request within ReviewForm.js

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -18,23 +18,35 @@ const ReviewForm = () => {
   };
 
   return (
-    <div>
-      <h1 className="text-2xl">Product</h1>
-      <input
-        className="border rounded border-black"
-        placeholder="Enter product name"
-        onChange={(event) => handleChange(event, "productName")}
-      />
-      <h1 className="text-2xl">Rating</h1>
-      <StarRating starCount={5} />
-      <h1 className="text-2xl">Review</h1>
-      <textarea
-        className="border rounded border-black"
-        placeholder="Optional..."
-        onChange={(event) => handleChange(event, "productReview")}
-      />
-      <div>
-        <button onClick={submitReview}>Submit Review</button>
+    <div className="grid grid-cols-2 gap-6 mx-16">
+      <form className="flex flex-col space-y-3">
+        <h2 className="text-2xl">Product</h2>
+        <input
+          className="border rounded border-black p-2"
+          placeholder="Enter product name"
+          onChange={(event) => handleChange(event, "productName")}
+        />
+        <h2 className="text-2xl">Rating</h2>
+        <StarRating starCount={5} />
+        <h2 className="text-2xl">Review</h2>
+        <textarea
+          className="border rounded border-black p-2 h-36"
+          placeholder="Optional..."
+          onChange={(event) => handleChange(event, "productReview")}
+        />
+        <div>
+          <button
+            className="shadow-md disabled:opacity-50 border bg-primary px-5 py-2 rounded-full mt-4 text-white transition hover:bg-yellow-400"
+            type="submit"
+            onClick={submitReview}
+          >
+            Submit Review
+          </button>
+        </div>
+      </form>
+
+      <div className="bg-purple-600 flex justify-start items-start p-4">
+        <div className="bg-blue-300 w-56 h-56 rounded-lg"></div>
       </div>
     </div>
   );
