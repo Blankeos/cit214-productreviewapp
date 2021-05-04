d1df98eaaffb2349ed669aa739727c715c2e6773
Blankeos <carloantonioct@gmail.com>
2021-05-04 08:55:11 +0800

    Added a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-darkGray h-20 w-full text-white flex items-center justify-center">
+      2021 © Cafe.ly. All Rights Reserved.
+    </div>
+  );
+}
