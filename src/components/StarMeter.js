521c7018f5118f9db46e201596d6668463c77249
Blankeos <carloantonioct@gmail.com>
2021-05-06 04:23:08 +0800

    More StarMeter props for sizing

--- src/components/StarMeter.js
+++ src/components/StarMeter.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { IconContext } from "react-icons";
 import { FaStar } from "react-icons/fa";
 
 export default function StarMeter({
   rating = 0,
+  iconSize = "1em",
   lightClass = "text-gray-200",
   shadeClass = "text-primary",
   shadeIcon = <FaStar />,
@@ -14,26 +16,28 @@ export default function StarMeter({
     <>
       {/* Stars */}
       <div className="flex">
-        <div className={`relative flex self-start ${lightClass}`}>
-          {lightIcon}
-          {lightIcon}
-          {lightIcon}
-          {lightIcon}
-          {lightIcon}
-          {/* Stars Inner */}
-          <div
-            className="absolute top-0 left-0 overflow-hidden whitespace-nowrap"
-            style={{ width: ratingPercent }}
-          >
-            <div className={`flex w-24 ${shadeClass}`}>
-              {shadeIcon}
-              {shadeIcon}
-              {shadeIcon}
-              {shadeIcon}
-              {shadeIcon}
+        <IconContext.Provider value={{ size: iconSize }}>
+          <div className={`relative flex self-start ${lightClass}`}>
+            {lightIcon}
+            {lightIcon}
+            {lightIcon}
+            {lightIcon}
+            {lightIcon}
+            {/* Stars Inner */}
+            <div
+              className="absolute top-0 left-0 overflow-hidden whitespace-nowrap"
+              style={{ width: ratingPercent }}
+            >
+              <div className={`flex w-max ${shadeClass}`}>
+                {shadeIcon}
+                {shadeIcon}
+                {shadeIcon}
+                {shadeIcon}
+                {shadeIcon}
+              </div>
             </div>
           </div>
-        </div>
+        </IconContext.Provider>
       </div>
     </>
   );
