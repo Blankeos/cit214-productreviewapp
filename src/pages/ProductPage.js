3899bfab1fc9033d4d90c3b46dac03e7e4a7014e
Blankeos <carloantonioct@gmail.com>
2021-05-09 00:13:26 +0800

    Working API for Individual Product Page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import PageContainer from "../components/PageContainer";
 
 const ProductPage = () => {
@@ -25,11 +26,12 @@ const ProductPage = () => {
   }, []);
 
   return (
-    <PageContainer className="bg-purple-200">
+    <PageContainer className="bg-purple-200 text-gray-800">
       <div className="max-w-6xl mx-auto bg-purple-300">
         <div className="flex bg-red-500">
-          <div className="w-96 h-96 bg-gray-300 overflow-hidden">
-            {product.images ? (
+          {/* Image */}
+          <div className="flex flex-shrink-0 justify-center items-center w-96 h-96 bg-gray-300 overflow-hidden">
+            {product ? (
               <img
                 className="bg-gray-300 object-center object-cover w-full h-96"
                 src={product.images[0]}
@@ -39,9 +41,21 @@ const ProductPage = () => {
               "Loading Image..."
             )}
           </div>
-          <div className="flex flex-col">
-            <div>Product name</div>
-            <div>Product Description</div>
+          {/* Body */}
+          <div className="bg-primary flex flex-col py-12 px-12 justify-between w-full">
+            <div className="flex flex-col space-y-8">
+              <h1 className="font-extrabold text-5xl">
+                {product ? product.name : "Loading Product Name..."}
+              </h1>
+              <p>
+                {product
+                  ? product.description
+                  : "Loading Product Description..."}
+              </p>
+            </div>
+            <Link to="/review/productId">
+              <button className="default-btn">Review this product</button>
+            </Link>
           </div>
         </div>
       </div>
