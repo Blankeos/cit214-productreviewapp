97ecf624cd6dba1623cd3cc4d7a0ceffeca33e17
Blankeos <carloantonioct@gmail.com>
2021-05-16 19:20:11 +0800

    Fixed responsive issues and added skeleton animations Products Page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -82,6 +82,8 @@ const Products = () => {
                   <ProductCardSkeleton />
                   <ProductCardSkeleton />
                   <ProductCardSkeleton />
+                  <ProductCardSkeleton className="hidden lg:block" />
+                  <ProductCardSkeleton className="hidden lg:block" />
                 </>
               )}
             </div>
@@ -111,7 +113,7 @@ const Products = () => {
 export const ProductCard = ({ productData, ...rest }) => {
   return (
     <Link to={`/products/${productData._id}`}>
-      <div className="border border-gray-100 bg-white text-gray-800 flex flex-col relative group hover:text-white overflow-hidden">
+      <div className="border border-gray-100 bg-white text-gray-800 flex flex-col relative group  overflow-hidden h-full">
         {/* Product Image */}
         <div className="relative w-full h-80 sm:h-40 md:h-48">
           <div className="bg-gradient-to-t from-transparent via-transparent to-black opacity-70 w-full h-full absolute"></div>
@@ -139,8 +141,8 @@ export const ProductCard = ({ productData, ...rest }) => {
           <span className="text-white text-xs">4.1</span>
         </div>
         {/* Body */}
-        <div className="flex flex-col p-2 bg-white group-hover:bg-gray-900 transition-all duration-300">
-          <h3 className="font-bold text-sm">
+        <div className="flex flex-col p-2 bg-white group-hover:text-white flex-grow group-hover:bg-gray-900 transition-all duration-300">
+          <h3 className="relative font-bold text-sm">
             {productData.name ? productData.name : "No Name Found"}
           </h3>
           <p className="text-xs">18 Reviews</p>
@@ -151,29 +153,61 @@ export const ProductCard = ({ productData, ...rest }) => {
   );
 };
 
-export const ProductCardSkeleton = () => {
+export const ProductCardSkeleton = ({ className, ...rest }) => {
   return (
-    <div className="border border-gray-100 bg-white text-gray-800 flex flex-col relative group hover:text-white overflow-hidden">
-      {/* Product Image */}
-      <div className="relative w-full h-80 sm:h-40 md:h-48">
-        <div className="bg-gray-100 w-full h-full"></div>
-      </div>
-      {/* Stars */}
-      <div className="absolute top-0 right-0 p-2 flex space-x-2 items-center">
-        <StarMeter
-          iconSize="1.2em"
-          // shadeClass="text-yellow-400"
-          lightClass="text-gray-300"
-        />
-        <span className="text-gray-300 text-xs bg-gray-300 rounded">0.0</span>
-      </div>
-      {/* Body */}
-      <div className="p-2 bg-white">
-        <h3 className="font-bold text-sm bg-gray-400 text-gray-400 w-28 rounded">
-          Product Name
-        </h3>
-        <p className="my-1 rounded text-xs bg-gray-200 text-gray-200 w-16">s</p>
-        <p className="rounded text-xs bg-gray-200 text-gray-200 w-14">s</p>
+    <div className={`${className}`}>
+      <div className="select-none border border-gray-100 bg-white text-gray-800 flex flex-col relative group hover:text-white overflow-hidden">
+        {/* Product Image */}
+        <div className="relative w-full h-80 sm:h-40 md:h-48">
+          <div
+            className="bg-gray-200 w-full h-full animate-pulse"
+            style={{
+              animationDuration: "1s",
+              animationDelay: "0s",
+            }}
+          ></div>
+        </div>
+        {/* Stars */}
+        <div
+          className="absolute top-0 right-0 p-2 flex space-x-2 items-center animate-pulse"
+          style={{
+            animationDuration: "1s",
+            animationDelay: "0.4s",
+          }}
+        >
+          <StarMeter iconSize="1.2em" lightClass="text-gray-300" />
+          <span className="text-gray-300 text-xs bg-gray-300 rounded">0.0</span>
+        </div>
+        {/* Body */}
+        <div className="p-2 bg-white">
+          <h3
+            className="font-bold text-sm bg-gray-400 text-gray-400 w-28 rounded animate-pulse"
+            style={{
+              animationDuration: "1s",
+              animationDelay: "0.3s",
+            }}
+          >
+            Product Name
+          </h3>
+          <p
+            className="my-1 rounded text-xs bg-gray-200 text-gray-200 w-16 animate-pulse"
+            style={{
+              animationDuration: "1s",
+              animationDelay: "0.6s",
+            }}
+          >
+            s
+          </p>
+          <p
+            className="rounded text-xs bg-gray-200 text-gray-200 w-14 animate-pulse"
+            style={{
+              animationDuration: "1s",
+              animationDelay: "0.9s",
+            }}
+          >
+            s
+          </p>
+        </div>
       </div>
     </div>
   );
