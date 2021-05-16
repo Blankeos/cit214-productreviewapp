80ff66e0b32aef408d3fffef936c92e50633c94e
Blankeos <carloantonioct@gmail.com>
2021-05-17 05:30:01 +0800

    easy util funcs for get product api requests

--- src/services/restServices.js
+++ src/services/restServices.js
@@ -12,6 +12,43 @@ export const getProfile = async (createToken) => {
   }
 };
 
+export const getAllProducts = async () => {
+  const url = "/api/products";
+  try {
+    const res = await axios.get(url);
+    const results = res.data.map((product) => {
+      return {
+        ...product,
+      };
+    });
+    return results;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getOneProduct = async (productID) => {
+  const url = `/api/products/${productID}`;
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// idk obsolete lang ni guro...
+export const searchProductWithText = async (createToken, inputText) => {
+  const url = `/api/products?name=${inputText}`;
+  const header = await createToken();
+  try {
+    const res = await axios.get(url, header);
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 // POST requests
 export const addNewUser = async (createToken, email, password, displayName) => {
   const url = "/api/register";
