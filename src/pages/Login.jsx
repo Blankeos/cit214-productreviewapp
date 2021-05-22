447ef1389f9f8ebef76ec6ca5c938d8df5e2a2d1
Blankeos <carloantonioct@gmail.com>
2021-05-23 03:22:50 +0800

    Removed comments unneeded comments in Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
 
-// Goal is to have input fields for email + password
-// buttons for login, register, login w/ google account
-
 const Login = () => {
   return <LoginForm />;
 };
