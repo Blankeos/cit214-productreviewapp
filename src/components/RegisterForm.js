a789df62c0c0dfa40fa295f49e10edf51cab79f0
Blankeos <carloantonioct@gmail.com>
2021-05-23 03:25:33 +0800

    Removed massive unnecessary comments on RegisterForm.js

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -173,65 +173,6 @@ const RegisterForm = () => {
         </div>
       </div>
     </>
-    // <>
-    //   {authStateChecked && (
-    //     <div>
-    //       <h1 className="text-4xl text-yellow-500 text-center font-extrabold">
-    //         Register
-    //       </h1>
-    //       <p className="text-center p-4">
-    //         {currentUser && "Currently logged in: " + currentUser.email}
-    //       </p>
-    //       <form className="relative w-96 mx-auto p-12 border border-gray-300 rounded-md h-auto bg-white">
-    //         {error && (
-    //           <p className="text-center p-4 text-red-600 bg-red-200 w-72 rounded-lg  justify-self mb-4 text-sm">
-    //             {error}
-    //           </p>
-    //         )}
-    //         <div className="flex flex-col space-y-3">
-    //           <label>Email</label>
-    //           <input
-    //             className="border border-gray-300 rounded-sm p-2"
-    //             name="email"
-    //             type="email"
-    //             placeholder="Enter your email here"
-    //             onChange={(event) => handleChange(event, "email")}
-    //           ></input>
-    //           <label>Password</label>
-    //           <input
-    //             className="border border-gray-300 rounded-sm p-2"
-    //             name="password"
-    //             type="password"
-    //             placeholder="Enter your password here"
-    //             onChange={(event) => handleChange(event, "password")}
-    //           ></input>
-    //         </div>
-    //         <p className="absolute text-right p-4 text-green-600 rounded-lg justify-content text-sm right-8 w-44">
-    //           <p className="text-red-500">
-    //             {passwordStrength.length < 6 &&
-    //               passwordStrength.length > 0 &&
-    //               "Bad password"}
-    //           </p>
-    //           <p className="text-yellow-500">
-    //             {passwordStrength.length >= 6 &&
-    //               passwordStrength.length < 10 &&
-    //               "Good"}
-    //           </p>
-    //           {passwordStrength.length >= 10 && "Great"}
-    //         </p>
-
-    //         <button
-    //           className="default-btn"
-    //           type="submit"
-    //           onClick={(e) => submit(e)}
-    //           disabled={loading}
-    //         >
-    //           {loading ? <AnimatedLoadingIcon size="1.4em" /> : "Register"}
-    //         </button>
-    //       </form>
-    //     </div>
-    //   )}
-    // </>
   );
 };
 
