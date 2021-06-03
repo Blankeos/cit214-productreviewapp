96da7bf7ad80ba8faf98d30fb7a00c0aef044a0f
Carlo Taleon <38070918+Blankeos@users.noreply.github.com>
2021-06-04 00:01:53 +0800

    Added display for "No product found" in search
    3rd attempt to fix deployment issues

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -116,22 +116,21 @@ const Products = () => {
           <div className="flex-grow w-8/12">
             {/* Product Grid */}
             <div className="px-4 py-4 shadow-md rounded-2xl border border-gray-100 overflow-hidden bg-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full">
-              {queriedProducts ? 
-                queriedProducts.length > 0 ? (
+              {queriedProducts ? (
+                queriedProducts.length ? (
                   queriedProducts.slice(0, 10).map((product) => {
                     return (
                       <ProductCard
-                        key={product.item._id}
-                        productData={product.item}
+                      key={product.item._id}
+                      productData={product.item}
                       />
                     );
-                  }) 
-                  :
-                  (
-                    <>
-                      <p className="text-gray-500 col-span-full">No products of this name found :(</p>
-                    </>
-                  )
+                  })
+                ) : (
+                  <p className="text-gray-500 col-span-full">
+                    No products of this name found :(
+                  </p>
+                )
               ) : (
                 <>
                   <ProductCardSkeleton />
@@ -141,7 +140,7 @@ const Products = () => {
                   <ProductCardSkeleton className="hidden lg:block" />
                   <ProductCardSkeleton className="hidden lg:block" />
                 </>
-              )}
+             )}
             </div>
           </div>
           <div className="break md:hidden" />
