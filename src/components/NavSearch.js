bc6e4f2f4f12d862574df790d8ed75d5ff68cac5
Carlo Taleon <38070918+Blankeos@users.noreply.github.com>
2021-06-04 22:45:10 +0800

    Smaller text in navsearch

--- src/components/NavSearch.js
+++ src/components/NavSearch.js
@@ -38,7 +38,7 @@ const NavSearch = (props) => {
           </Link>
           <div className="bg-primary md:bg-transparent flex flex-col space-y-4 top-0 left-0 py-5 pt-20 absolute w-full items-center md:relative md:flex-row md:items-center md:mt-0 md:space-x-4 md:space-y-0 md:py-0 md:pb-0 md:focus-within:pb-0 pb-40 focus-within:pb-10 rounded-b-2xl transition-all">
             {/* Search Box */}
-            <div className="flex bg-white transition-all duration-300 ease-out focus-within:w-11/12 md:focus-within:w-full focus-within:shadow-lg py-2 px-2.5 md:w-48 rounded-full space-x-1 md:border md:border-gray-300 items-center w-10/12">
+            <div className="text-sm flex bg-white transition-all duration-300 ease-out focus-within:w-11/12 md:focus-within:w-full focus-within:shadow-lg py-2 px-2.5 md:w-48 rounded-full space-x-1 md:border md:border-gray-300 items-center w-10/12">
               <RiSearch2Line size="1.2em" color="gray" />
               <div ref={node} className="w-full flex space-x-1">
                 <input
