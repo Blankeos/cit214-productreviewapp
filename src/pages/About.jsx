cb487e85316b3c4a3a58290674b29ea94dacb348
Blankeos <carloantonioct@gmail.com>
2021-06-14 07:48:20 +0800

    Complete removed-bg pictures for Team Members

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -32,11 +32,11 @@ const About = () => {
           <div className="text-gray-800  mx-auto max-w-6xl flex-grow flex flex-col items-center pt-16 px-2 sm:px-8 pb-24">
             <h2 className="font-extrabold text-5xl m-8">Meet the Team</h2>
             {/* Grids of the people */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 w-full p-5 px-20 sm:px-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 w-full p-5 sm:px-20">
               <TeamMember
                 name="Carlo Taleon"
                 title="Lead Developer"
-                imageSource="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/170753804_4249592568426660_2396136441650263141_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeEv8XiL6KQkh3YwT6HAVmfjTWnQUnBKrlFNadBScEquUfaIAoBbfLIw80FRWD2ElQAtn2vH3NU8TjGynTzH1ir8&_nc_ohc=1V4tkHtMkgcAX8C-HFJ&_nc_ht=scontent.fceb2-1.fna&oh=63d824ca599fdc236d2abdc3159b7eea&oe=60C3A8AB"
+                imageSource="https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/team_images/lead_developer.png"
               />
               <TeamMember
                 name="CJ Rubinos"
@@ -49,17 +49,17 @@ const About = () => {
               <TeamMember
                 name="Jiezel Maglalang"
                 title="UI/UX Designer"
-                imageSource="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/142299667_240387301011197_6516933063339469195_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=174925&_nc_eui2=AeGYv4Gk_nB43Fg5VPrwMuzukYCMAvI8PCqRgIwC8jw8KvHp7rNwyO0rjPM6CAnUYar6eg_V-4TkMsl4UWOk-IKo&_nc_ohc=F5yOkeTZHW0AX_jZFm5&_nc_ht=scontent.fceb2-1.fna&oh=52f52266c3c8d2db1738eb49489a26cc&oe=60C3A6D1"
+                imageSource="https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/team_images/ui_ux_designer.png"
               />
               <TeamMember
                 name="Gene Caleb Carbonilla"
                 title="Data Specialist"
-                imageSource="https://scontent.fceb2-2.fna.fbcdn.net/v/t1.6435-9/152552229_4423375211011385_137838467295572734_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHZgiHL-NItDp6AZC2nOPlMY096oYGRvi9jT3qhgZG-L9ldHWMj5MhkdVMsjLDtoN2GQ5VdpYzAZ2vHnCgW5eCM&_nc_ohc=FHaDOtDbSOEAX-UaJyw&_nc_ht=scontent.fceb2-2.fna&oh=604233eb891c1298936abb0bd336e2fd&oe=60C38714"
+                imageSource="https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/team_images/data_specialist.png"
               />
               <TeamMember
                 name="Xyphrus Von Keith Caguan"
                 title="UX Designer"
-                imageSource="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/119425146_342515593617256_1883148394415592026_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGjiWucsq2MRgd1rQ1ROMHh1sM6xMA7ZHHWwzrEwDtkceALSabOTRaY36B_2u6qTzrsZktKtwmZgViQfKSXWiqd&_nc_ohc=pFhx5JlyAwAAX8QsTJd&_nc_ht=scontent.fceb2-1.fna&oh=04194033549975008845af01b017637f&oe=60C4B65A"
+                imageSource="https://raw.githubusercontent.com/seajayrubynose/cafely-pictures/master/team_images/ux_designer.png"
               />
               <TeamMember
                 name="Zhyray Remigio"
@@ -77,7 +77,7 @@ const About = () => {
 const TeamMember = ({ name, title, imageSource, ...rest }) => {
   return (
     <div className="flex flex-col h-96">
-      <div className="w-full flex-grow overflow-hidden relative">
+      <div className="w-full flex-grow flex-shrink-0 overflow-hidden relative">
         {/* Background */}
         <div className="absolute bottom-0 w-full h-5/6 bg-gradient-to-tl from-primary to-yellow-300"></div>
         {/* Image */}
@@ -85,7 +85,7 @@ const TeamMember = ({ name, title, imageSource, ...rest }) => {
           className="w-full h-full absolute"
           style={{
             backgroundImage: `url(${imageSource})`,
-            backgroundPosition: "center",
+            backgroundPosition: "bottom",
             backgroundSize: "contain",
             backgroundRepeat: "no-repeat",
           }}
